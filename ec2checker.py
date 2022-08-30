a799d3cde4eeeb11650516cf309958261e53d8b6
Ishay Slutzki <ssishay@gmail.com>
2022-08-30 17:52:30 +0300

    Finalized script (hopefully)

--- ec2checker.py
+++ ec2checker.py
@@ -1,28 +1,47 @@
 import boto3
+import sys
+import time
 
-print("Hello world")
-print(boto3.__version__)
+def scan_instances():
+    ec2 = boto3.resource('ec2')
 
-ec2 = boto3.resource('ec2')
-#instance = ec2.Instance('id')
+#### In order to scan by other tags you can change or add Name and Value pairs
+    instance_iterator = ec2.instances.filter(
+        Filters=[
+            {
+                'Name': 'tag:k8s.io/role/master',
+                'Values': [
+                    '1'
+                ],
+                'Name': 'instance-state-code',
+                'Values': [
+                    '16'
+                ]
+            },
+        ],
+        DryRun=False,
+        MaxResults=10
+    )
 
-instance_iterator = ec2.instances.filter(
-    Filters=[
-        {
-            'Name': 'tag:k8s.io/role/master',
-            'Values': [
-                '1'
-            ],
-            'Name': 'instance-state-code',
-            'Values': [
-                '16'
-            ]
-        },
-    ],
-    DryRun=False,
-    MaxResults=10
-)
+#    inst_names = [tag['Value'] for i in instance_iterator for tag in i.tags if tag['Key'] == 'Name']
 
-inst_names = [tag['Value'] for i in instance_iterator for tag in i.tags if tag['Key'] == 'Name']
-print(inst_names)
-print("***")
+    print("***")
+    for instance in instance_iterator:
+        ec2_name = [tag['Value'] for tag in instance.tags if tag['Key'] == 'Name']
+        ec2_ip = instance.public_ip_address
+        ec2_type = instance.instance_type
+        print("Instance name: {0}\nIP address: {1}\nType: {2}\n***".format(ec2_name, ec2_ip, ec2_type))
+
+# Main
+try:
+    interval = sys.argv[1]
+except IndexError:
+    interval = None
+
+if interval == None:
+    scan_instances()
+else:
+    while True:
+        scan_instances()
+        print("Sleeping for {0} seconds!".format(interval))
+        time.sleep(int(interval))
